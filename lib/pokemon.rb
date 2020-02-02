0419abbd8b12a7962885ad2d794d37331d2bebb2
Anthony Mai <anthony.mai530@gmail.com>
2020-02-02 18:49:55 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,8 @@ class Pokemon
     SELECT * FROM pokemon
     WHERE id = (?)
     SQL
-    pokemon = db.execute(sql, [id]).flatten
+    pokemon = db.execute(sql, id).flatten
+    Pokemon.new = (id, pokemon[1], pokemon[2], db)
   end
 
 
